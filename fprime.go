0aca454f8abf1c93b40415e35f34fd723d50badf
Abaisky <52810981+Abaisky@users.noreply.github.com>
2020-02-23 11:32:03 +0600

    init: Create go-fprime.go

---
+++ fprime.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+    "fmt"
+    "os"
+    "strconv"
+)
+
+func main() {
+    args := os.Args[1:]
+
+    if len(args) == 1 {
+        count := 0
+        num := 0
+        num, _ = strconv.Atoi(args[0])
+        l := PrimeFactors(num)
+        fmt.Println(l)
+
+        for i := 0; i < len(l); i++ {
+            count++
+            fmt.Print("*")
+            fmt.Print(l[i])
+        }
+        fmt.Println()
+
+    }
+}
+
+func PrimeFactors(x int) []rune {
+    factors := []rune{}
+
+    candidate := 2
+    for candidate <= x {
+        if x%candidate == 0 {
+            factors = append(factors, rune(candidate))
+
+            x = x / candidate
+        } else {
+            candidate++
+        }
+    }
+    // fmt.Println(factors)
+    return factors
+}

9e1173c49ba178b9fe700cbe8e27e8208595422e
Abaisky <abay87072104545@gmail.com>
2020-03-12 22:40:15 +0600

    Add itoaprog and atoibaseprog

---
+++ 14atoibaseprog/main.go
@@ -0,0 +1,134 @@
+package main
+
+import "fmt"
+
+///////////////////////////////////////////////////////////////////////////////
+// atoibaseprog
+// allowedFunctions	[…]
+// 0	--cast
+// 1	len
+// WARNING! VERY IMPORTANT!
+
+// For this exercise a function will be tested with the exam own main. However the student still needs to submit a structured program:
+
+// This means that:
+
+//     The package needs to be named package main.
+//     The submitted code needs one declared function main(func main()) even if empty.
+//     The function main declared needs to also pass the Restrictions Checker(illegal functions tester). It is advised for the student to just empty the function main after its own testings are done.
+//     Every other rules are obviously the same than for a program.
+
+// Instructions
+
+// Write a function that takes a string number and its string base in parameters and returns its conversion as an int.
+
+// If the base or the string number is not valid it returns 0
+
+// Validity rules for a base :
+
+//     A base must contain at least 2 characters.
+//     Each character of a base must be unique.
+//     A base should not contain + or - characters.
+
+// Only valid string numbers will be tested.
+
+// The function does not have to manage negative numbers.
+// Expected function
+
+// func AtoiBase(s string, base string) int {
+
+// }
+
+// Usage
+
+// Here is a possible program to test your function :
+
+// package main
+
+// import (
+// 	"fmt"
+// )
+
+// func main() {
+// 	fmt.Println(AtoiBase("125", "0123456789"))
+// 	fmt.Println(AtoiBase("1111101", "01"))
+// 	fmt.Println(AtoiBase("7D", "0123456789ABCDEF"))
+// 	fmt.Println(AtoiBase("uoi", "choumi"))
+// 	fmt.Println(AtoiBase("bbbbbab", "-ab"))
+// }
+
+// And its output :
+
+// student@ubuntu:~/test$ go build
+// student@ubuntu:~/test$ ./test
+// 125
+// 125
+// 125
+// 125
+// 0
+// student@ubuntu:~/test$
+/////////////////////////////////////////////////////////
+
+func main() {
+	fmt.Println(AtoiBase("125", "0123456789"))
+	fmt.Println(AtoiBase("1111101", "01"))
+	fmt.Println(AtoiBase("7D", "0123456789ABCDEF"))
+	fmt.Println(AtoiBase("uoi", "choumi"))
+	fmt.Println(AtoiBase("bbbbbab", "-ab"))
+}
+
+func AtoiBase(s string, base string) int {
+	if !isValid(base) {
+		return 0
+	}
+	res := 0
+	for _, x := range s {
+		for i, y := range base {
+			if x == y {
+				res = res*len(base) + i
+			}
+		}
+	}
+	return res
+}
+func isValid(s string) bool {
+	if s[0] == '-' || s[0] == '+' {
+		return false
+	}
+	for i := 0; i < len(s); i++ {
+		for j := i + 1; j < len(s); j++ {
+			if s[i] == s[j] {
+				return false
+			}
+		}
+	}
+	return true
+}
+
+// func AtoiBase(s string, base string) int {
+// 	result := 0
+// 	ArrBase := []rune(base)
+
+// 	if len(ArrBase) < 2 {
+// 		return 0
+// 	}
+// 	for _, i := range base {
+// 		count := 0
+// 		for _, j := range base {
+// 			if i == j {
+// 				count++
+// 			}
+// 			if count > 1 || j == '-' || j == '+' {
+// 				return 0
+// 			}
+// 		}
+// 	}
+// 	for _, a := range s {
+// 		for i := 0; i < len(ArrBase); i++ {
+// 			if a == ArrBase[i] {
+// 				result = result*len(ArrBase) + i
+// 			}
+// 		}
+// 	}
+// 	return result
+// }
---
+++ 14itoaprog/main.go
@@ -0,0 +1,58 @@
+//////////////////////////////////////////////////////////////////////
+// itoaprog
+// allowedFunctions	[…]
+// 0	len
+// 1	make
+// 2	--cast
+// ##WARNING! VERY IMPORTANT!
+
+// For this exercise a function will be tested with the exam own main. However the student still needs to submit a structured program:
+
+// This means that:
+
+//     The package needs to be named package main.
+//     The submitted code needs one declared function main(func main()) even if empty.
+//     The function main declared needs to also pass the Restrictions Checker(illegal functions tester). It is advised for the student to just empty the function main after its own testings are done.
+//     Every other rules are obviously the same than for a program.
+
+// Instructions
+
+//     Write a function that simulates the behaviour of the Itoa function in Go. Itoa transforms a number represented as an int in a number represented as a string.
+
+//     For this exercise the handling of the signs + or - does have to be taken into account.
+
+// Expected function
+
+// func Itoa(n int) string {
+
+// }
+///////////////////////////////////////////////////////////////////
+
+package main
+
+import "fmt"
+
+func main() {
+	x := -9223372036854775808
+	y := Itoa(x)
+	fmt.Println(y)
+}
+
+func Itoa(n int) string {
+	nbr := n
+	result := ""
+	for n != 0 {
+		index := n % 10
+		fmt.Println(index)
+		if index < 0 {
+
+			index *= -1
+		}
+		result = string(index+48) + result
+		n /= 10
+	}
+	if nbr < 0 {
+		result = "-" + result
+	}
+	return result
+}
